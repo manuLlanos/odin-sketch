926ea31f3478eea1a0dd6b9c7438bb1070fc430f
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-16 20:31:43 -0300

    Put canvas creation code into own function

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ const container = document.querySelector("#container");
 const SIZE = 16;
 const WIDTH = 960;
 
-for (let i = 0; i < SIZE; i++) {
-    let row = document.createElement("div");
-    row.className = "row";
+function createCanvas(size) {
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
 
-    for (let j = 0; j < SIZE; j++) {
-        let square = document.createElement("div");
-        square.className = "square";
-        square.style.height = `${WIDTH/SIZE}px`;
-        square.style.width = `${WIDTH/SIZE}px`;
-        square.addEventListener("mouseenter", () => { square.classList.toggle("filled")});
-        row.appendChild(square);
+        for (let j = 0; j < size; j++) {
+            let square = document.createElement("div");
+            square.className = "square";
+            square.style.height = `${WIDTH / size}px`;
+            square.style.width = `${WIDTH / size}px`;
+            square.addEventListener("mouseenter", () => { square.classList.toggle("filled") });
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
     }
 
-    container.appendChild(row);
 }
+
+createCanvas(SIZE);
