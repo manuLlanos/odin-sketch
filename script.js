a6920740d9927483d5dd67c86015e64a4822483e
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-16 20:26:45 -0300

    Make square size be determined by script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
 
 const SIZE = 16;
+const WIDTH = 960;
 
 for (let i = 0; i < SIZE; i++) {
     let row = document.createElement("div");
@@ -9,6 +10,8 @@ for (let i = 0; i < SIZE; i++) {
     for (let j = 0; j < SIZE; j++) {
         let square = document.createElement("div");
         square.className = "square";
+        square.style.height = `${WIDTH/SIZE}px`;
+        square.style.width = `${WIDTH/SIZE}px`;
         square.addEventListener("mouseenter", () => { square.classList.toggle("filled")});
         row.appendChild(square);
     }
