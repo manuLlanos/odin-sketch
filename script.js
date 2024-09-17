47709c5d36a662310a2feaeaac322ae91b988129
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-16 21:54:18 -0300

    Make new canvas button ask user for size and then create canvas dynamically

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
+const newCanvasButton = document.querySelector("#btn-new");
 
-const SIZE = 16;
+const DEFAULT_SIZE = 16;
 const WIDTH = 960;
 
 function createCanvas(size) {
@@ -11,8 +12,8 @@ function createCanvas(size) {
         for (let j = 0; j < size; j++) {
             let square = document.createElement("div");
             square.className = "square";
-            square.style.height = `${WIDTH / size}px`;
-            square.style.width = `${WIDTH / size}px`;
+            square.style.height = `${Math.floor(WIDTH / size)}px`;
+            square.style.width = `${Math.floor(WIDTH / size)}px`;
             square.addEventListener("mouseenter", () => { square.classList.toggle("filled") });
             row.appendChild(square);
         }
@@ -22,4 +23,26 @@ function createCanvas(size) {
 
 }
 
-createCanvas(SIZE);
+function askUserForSize() {
+    let sizeInput = 0;
+    do {
+        sizeInput = parseInt(prompt("Canvas size? (max:100)", "16"));
+    }
+    while(sizeInput < 0 || sizeInput > 100);
+
+    deleteRows();
+
+    createCanvas(sizeInput);
+
+}
+
+function deleteRows() {
+    while(container.firstChild) {
+        console.log("h");
+        container.removeChild(container.firstChild);
+    }
+}
+
+newCanvasButton.addEventListener("click", askUserForSize);
+
+createCanvas(DEFAULT_SIZE);
